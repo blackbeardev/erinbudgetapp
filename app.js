6815f6b60a4c6bc3cd79982877ea64d4a3d65546
flyingfoxweb <erin@flyingfox.net.nz>
2017-11-21 23:23:39 +0000

    Added the calculation functions for calculating the totals for expenses and incomes, the overall total of income - expenses, and the percentage of expenses.

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ var budgetController = (function () {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -24,7 +32,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -51,6 +61,30 @@ var budgetController = (function () {
            data.allItems[type].push(newItem);
            //Return the new element:
            return newItem;
+        },
+        calculateBudget: function() {
+            //calculate total income and expenses:
+            calculateTotal("exp");
+            calculateTotal("inc");
+
+            //calculate the budget = income - expenses:\
+            data.budget = data.totals.inc - data.totals.exp;
+
+            //calculate the percentage of income that has been spent:
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
         testing: function() {
             console.log(data);
@@ -153,11 +187,15 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     //Update the budget:
     var updateBudget = function() {
+        
         //Calculate the budget:
+        budgetCtrl.calculateBudget();
 
         //Return the budget:
+        var budget = budgetCtrl.getBudget();
 
         //Display the budget on the UI:
+        console.log(budget);
     };
 
     
